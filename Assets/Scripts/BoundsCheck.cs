2f151cfdaa287479267713b7581d7bcb319a547d
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2020-09-29 20:19:44 -0400

    Added boundscheck for enemy_0

--- Assets/Scripts/BoundsCheck.cs
+++ Assets/Scripts/BoundsCheck.cs
@@ -20,17 +20,23 @@ public class BoundsCheck : MonoBehaviour
 
         public float radius = 1f;
 
-
+        public bool     keepOnScreen = true;
 
         [Header("Set Dynamically")]
+
+        public bool isOnScreen = true;
 
         public float camWidth;
 
         public float camHeight;
 
+        [HideInInspector]
+
+        public bool offRight, offLeft, offUp, offDown;
 
 
-        void Awake()
+
+    void Awake()
         {
 
             camHeight = Camera.main.orthographicSize;                            
@@ -46,12 +52,20 @@ public class BoundsCheck : MonoBehaviour
 
             Vector3 pos = transform.position;
 
+            isOnScreen = true;
+
+            offRight = offLeft = offUp = offDown = false;
 
 
-            if (pos.x > camWidth - radius)
+
+        if (pos.x > camWidth - radius)
             {
 
                 pos.x = camWidth - radius;
+
+                isOnScreen = false;
+
+                offRight = true;
 
             }
 
@@ -63,7 +77,10 @@ public class BoundsCheck : MonoBehaviour
 
                 pos.x = -camWidth + radius;
 
-            }
+                isOnScreen = false;
+
+                offLeft = true;
+            }   
 
 
 
@@ -72,6 +89,12 @@ public class BoundsCheck : MonoBehaviour
 
                 pos.y = camHeight - radius;
 
+                isOnScreen = false;
+
+                offUp = true;
+
+                
+
             }
 
             if (pos.y < -camHeight + radius)
@@ -79,11 +102,28 @@ public class BoundsCheck : MonoBehaviour
 
                 pos.y = -camHeight + radius;
 
+                isOnScreen = false;
+
+                offDown = true;
+
             }
 
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+
+        if (keepOnScreen && !isOnScreen)
+        {                                
+
+            transform.position = pos;                                       
+
+            isOnScreen = true;
+
+            offRight = offLeft = offUp = offDown = false;
+
+        }
 
 
-            transform.position = pos;
+
+        transform.position = pos;
 
         }
 
