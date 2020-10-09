8bf1fc7beb4e44de5f26588e2e95583338311077
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2020-10-09 11:28:16 -0400

    Copied menu Scene from SHMUP++ branch and added an animation to ship in shop screen

---
+++ Assets/Scripts/Menu_Scripts/HeroModelDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroModelDisplay : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public Vector3 point1;
+    public Vector3 point2;
+
+    public float speed;
+
+    [Header("Set Dynamically")]
+
+    public float startTime;
+    public float pointsDistance;
+
+// Start is called before the first frame update
+
+    void Start()
+    {
+        startTime = Time.time;
+
+        pointsDistance = Vector3.Distance(point1, point2);
+    }
+    
+
+
+
+
+
+// Update is called once per frame
+    void Update()
+    {
+
+    float distCovered = (Time.time - startTime) * speed;
+
+    float t = distCovered / pointsDistance;
+        
+
+        transform.localPosition = Vector3.Lerp(point1, point2, t);
+        Debug.Log(t);
+
+
+    }
+
+}
+
---
+++ Assets/Scripts/Menu_Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GlobalControl s;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void SelectHero0()
+    {
+        s = GameObject.Find("GlobalObject").GetComponent<GlobalControl>();
+        s.SetButton(0);
+        Debug.Log("Set button to 0");
+    }
+
+    public void SelectHero1()
+    {
+        s = GameObject.Find("GlobalObject").GetComponent<GlobalControl>();
+        s.SetButton(1);
+        Debug.Log("Set button to 1");
+    }
+
+    public void SelectHero2()
+    {
+        s = GameObject.Find("GlobalObject").GetComponent<GlobalControl>();
+        s.SetButton(2);
+        Debug.Log("Set button to 2");
+    }
+
+
+}
